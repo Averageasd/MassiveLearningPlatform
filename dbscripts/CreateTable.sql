2141abbf87faee47e20d205cc4fa44c6d263174b
Nguyen Huynh <nhuynh9497@gmail.com>
2025-06-11 16:57:23 -0400

    added backup file and db scripts.

---
+++ dbscripts/CreateTable.sql
@@ -0,0 +1,17 @@
+IF OBJECT_ID(N'User',N'U') IS NULL
+BEGIN
+	CREATE TABLE [User] (
+		ID INT IDENTITY(1,1) PRIMARY KEY,
+		UserName NVARCHAR(20) UNIQUE NOT NULL,
+		Age INT NOT NULL,
+		UserPassword NVARCHAR(30) NOT NULL,
+		DateJoin DATETIME2 DEFAULT GETDATE()
+	);
+	ALTER TABLE [User]
+	ADD 
+		CONSTRAINT UserName_Check_Length CHECK (LEN(UserName) >= 5 AND LEN(UserName) <= 20),
+		CONSTRAINT Age_Check_Length CHECK (Age >= 0 AND Age <= 120),
+		CONSTRAINT Password_Check_Length CHECK (LEN(UserPassword) >= 8 AND LEN(UserPassword) <= 30);
+END
+
+
